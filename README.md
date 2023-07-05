84273203dad0ba4facd0fb8812d8343ff6f56294
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-07-05 20:49:00 +0100

    add source

---
+++ README.md
@@ -0,0 +1,1 @@
+# tnoss notes
---
+++ data/headerNavLinks.js
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'blog' },
+  { href: '/tags', title: 'tags' },
+  // { href: '/projects', title: 'projects' },
+  // { href: '/about', title: 'about' },
+]
+
+export default headerNavLinks
