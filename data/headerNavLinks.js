de8ebd1f3999cff4cda86bebaf9067684959d203
Truc Nguyen <truc.nguyentrung@hotmail.com>
2024-03-28 18:18:27 +0000

    init template

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'blog' },
-  { href: '/tags', title: 'tags' },
-  // { href: '/projects', title: 'projects' },
-  // { href: '/about', title: 'about' },
-]
-
-export default headerNavLinks
